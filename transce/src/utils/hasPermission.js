fdf78dfa05d0cd10397d6ec55f03b7bf53d3993e
sunlu <sunlu@moretickets.com>
2019-01-04 03:54:11 +0800

    按钮权限公有类

---
+++ transce/src/utils/hasPermission.js
@@ -0,0 +1,9 @@
+import store from '../store'
+
+// 用来控制按钮显示与否
+
+export function hasPermission(permission) {
+  const myBtns = store.getters.permissions
+  // console.log('按钮')
+  return myBtns.indexOf(permission) > -1
+}
